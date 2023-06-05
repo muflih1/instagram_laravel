17616f246427295c8e295a665a3554807ca7fb96
muflih1 <muflimuflih306@gmail.com>
2023-06-05 15:42:57 +0530

    added create post page

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,31 @@
 @extends('layout.layout')
 
 @section('content')
-    <h1>Create new post</h1>
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <h1 class="fs-3 mb-3">Create new post</h1>
+            <form action="{{ route('p.store') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <textarea 
+                        name="caption" 
+                        id="caption" 
+                        class="form-control" 
+                        placeholder="Write a caption.."
+                    >{{ old('caption') }}</textarea>
+                </div>
+                <div class="mb-3">
+                    <input
+                        type="file" 
+                        name="image" 
+                        id="image" 
+                        class="form-control"
+                    />
+                </div>
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary">Share</button>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
